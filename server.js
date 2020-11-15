212e7f4b950644d6d246f96c911807a7daa92cf8
saikiran <vasamsettisaikiran@yahoo.com>
2020-11-16 00:35:18 +0530

    dynmaic port added

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
-
 let app = express();
+let port = process.env.PORT;
 
 app.get('/', function(req, res){
     res.send("request received");
 });
 
-app.listen(3000);
+if(port == null || port == "")
+{
+    port = 3000;
+}
+
+app.listen(port);
